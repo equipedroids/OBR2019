1424c1e0124522ffdff8b5f7b4b26efd50da6c89
gigaDroid-Daniel <oficial.danieloliveira@gmail.com>
2019-08-24 22:24:03 -0300

    dahora

---
+++ LEFT_NANO.ino
@@ -0,0 +1,62 @@
+#include <Wire.h>
+#include <SparkFun_APDS9960.h>
+
+SparkFun_APDS9960 apds = SparkFun_APDS9960();
+uint16_t ambient = 0;
+uint16_t red = 0;
+uint16_t green = 0;
+uint16_t blue = 0;
+
+#define RANGE 10
+#define DEFAULT_GREEN 25
+#define DEFAULT_BLUE 25
+#define DEFAULT_RED 10
+#define PIN_JULIETA 11
+
+bool shouldPrint = false;
+
+void setup() {
+  Serial.begin(9600);
+  apds.init();
+  apds.enableLightSensor(false);
+  pinMode(PIN_JULIETA, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(2000);
+}
+
+void loop(){
+  apds.readAmbientLight(ambient);
+  apds.readRedLight(red);
+  apds.readGreenLight(green);
+  apds.readBlueLight(blue);
+
+  red = map(red, 0, 1024, 0, 255);
+  green = map(green, 0, 1024, 0, 255);
+  blue = map(blue, 0, 1024, 0, 255);
+
+  if(shouldPrint) serialPrint();
+  
+  if(
+  (red >= DEFAULT_RED-RANGE && red <= DEFAULT_RED+RANGE) &&
+  (green >= DEFAULT_GREEN-RANGE && green <= DEFAULT_GREEN+RANGE) &&
+  (blue >= DEFAULT_BLUE-RANGE && blue <= DEFAULT_BLUE+RANGE)
+  ){
+    if(shouldPrint) Serial.println("verde");
+    digitalWrite(PIN_JULIETA,HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(2000);
+  }
+  else{ 
+    if(shouldPrint) Serial.println("preto ou branco");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(PIN_JULIETA,LOW);
+  }
+}
+
+void serialPrint(){
+  Serial.print(red);
+  Serial.print("\t");
+  Serial.print(green);
+  Serial.print("\t");
+  Serial.println(blue);
+}
---
+++ OFICIAL.ino
@@ -0,0 +1,283 @@
+/* BIBLIOTECAS */
+#include <FalconRobot.h>
+#include <Wire.h>
+#include <MPU6050.h>
+
+/* CALIBRAGENS */
+#define DEFAULT_SPEED 32
+#define MAX_SPEED 45
+#define DEFAULT_DISTANCE 10
+#define DEFAULT_SENSIVITY 3
+#define ID_BLACK 975
+#define LEFT_WHITE 900
+#define RIGHT_WHITE 883
+#define DEFAULT_DEG 63
+
+/* MOTORES */
+FalconRobotMotors motors(5, 7, 6, 8);
+
+/* ULTRASSÔNICO */
+FalconRobotDistanceSensor distanceSensor(2, 3);
+int distance;
+
+/* GIROSCÓPIO */
+MPU6050 mpu;
+float yaw = 0;
+float lastYaw = 0;
+float timeStep = 0.01;
+
+/* SENSORES DE IDENTIFICAÇÃO */
+FalconRobotLineSensor leftId(A0);
+int leftIdValue;
+FalconRobotLineSensor middleId(A1);
+int middleIdValue;
+FalconRobotLineSensor rightId(A2);
+int rightIdValue;
+
+/* SENSORES DE SEGUE-LINHA */
+FalconRobotLineSensor left(A3);
+int leftValue;
+FalconRobotLineSensor right(A6);
+int rightValue;
+
+/* ARDUINOS EXTERNOS */
+#define LEFT_COLOR_NANO 11
+#define RIGHT_COLOR_NANO 12
+
+/* TEMPO */
+unsigned long int actualTime = 0;
+unsigned long int distanceLastTime = 0;
+unsigned long int motorStallLastTime = 0;
+
+/* FUNÇÃO INICIAL */
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G);
+  mpu.calibrateGyro();
+  mpu.setThreshold(DEFAULT_SENSIVITY);
+  pinMode(LEFT_COLOR_NANO, INPUT);
+  pinMode(RIGHT_COLOR_NANO, INPUT);
+  delay(2000);
+}
+
+/* FUNÇÃO PRINCIPAL */
+void loop() {
+  //actualTime = millis();
+  readLineSensors();
+  //printLineSensors();
+  
+  switch(isInTarget()){
+    case 123:
+      resolveIntersection();
+      moveTo("forward");
+      break;
+    case 12:
+      resolveIntersection();
+      moveTo("left");
+      break;
+    case 23:
+      resolveIntersection();
+      moveTo("right");
+      break;
+    case 45:
+      moveTo("forward");
+      break;
+    case 10:
+      lineFollower();
+      break;
+    case 0:
+      break;
+  }
+}
+
+/* FUNÇÃO PARA REALIZAR MOVIMENTOS  */
+void moveTo(String way){
+  if(way == "left"){
+    motors.leftDrive(DEFAULT_SPEED, BACKWARD);
+    motors.rightDrive(DEFAULT_SPEED, FORWARD);
+  }else if(way == "right"){
+    motors.leftDrive(DEFAULT_SPEED, FORWARD);
+    motors.rightDrive(DEFAULT_SPEED, BACKWARD);
+  }else if(way == "forward"){
+    motors.leftDrive(DEFAULT_SPEED, FORWARD);
+    motors.rightDrive(DEFAULT_SPEED, FORWARD);
+  }else if(way == "backward"){
+    motors.leftDrive(DEFAULT_SPEED, BACKWARD);
+    motors.rightDrive(DEFAULT_SPEED, BACKWARD);
+  }else if(way == "turn180"){
+    motors.leftDrive(DEFAULT_SPEED*2, FORWARD);
+    motors.rightDrive(DEFAULT_SPEED*2, BACKWARD);
+    delay(500);
+  }
+}
+
+/* FUNÇÃO DE LEITURA DOS SENSORES DE REFLECTÂNCIA */
+void readLineSensors(){
+  leftIdValue = leftId.read();
+  middleIdValue = middleId.read();
+  rightIdValue = rightId.read();
+  leftValue = left.read();
+  rightValue = right.read();
+}
+
+/* FUNÇÃO PARA CALIBRAGEM MANUAL DOS SENSORES DE REFLECTÂNCIA */
+void printLineSensors(){
+  Serial.print(leftIdValue);
+  Serial.print("\t");
+  Serial.print(middleIdValue);
+  Serial.print("\t");
+  Serial.print(rightIdValue);
+  Serial.print("\t");
+  Serial.print(leftValue);
+  Serial.print("\t");
+  Serial.println(rightValue);
+}
+
+/* FUNÇÃO PARA LEITURA DO SENSOR GIROSCÓPIO */
+void readGyroSensor(){
+  Vector norm = mpu.readNormalizeGyro();
+  yaw = yaw + norm.ZAxis * timeStep;
+}
+
+/* FUNÇÃO PARA EXECUTAR GIROS */
+void turnGyro(int deg, String way, bool higher){
+  readGyroSensor();
+  if(higher){
+    while(yaw < deg){
+      Serial.print(yaw);
+      Serial.print("\t");
+      Serial.println(deg);
+      readGyroSensor();
+      moveTo(way);
+    }
+  }else if(!higher){
+    while(yaw > deg){
+      Serial.print(yaw);
+      Serial.print("\t");
+      Serial.println(deg);
+      readGyroSensor();
+      moveTo(way);
+    }
+  }
+  readGyroSensor();
+  Serial.println(yaw);
+  lastYaw = yaw;
+  yaw = 0;
+}
+
+/* FUNÇÃO PARA IDENTIFICAR E DESVIAR DE OBSTÁCULOS */
+void deviateObstacles(){
+  distance = distanceSensor.read();
+  readGyroSensor();
+  if(distance >= 5 && distance <= DEFAULT_DISTANCE){
+    turnGyro(DEFAULT_DEG, "left", true);
+    motors.stop();delay(1000);
+    moveTo("forward"); delay(1000);
+    motors.stop();delay(1000);
+    turnGyro(-lastYaw, "right", false);
+    motors.stop();delay(1000);
+    moveTo("forward"); delay(1000);
+    motors.stop();delay(1000);
+    turnGyro(lastYaw, "right", false);
+    motors.stop();delay(1000);
+    readLineSensors();
+    while(middleIdValue <= ID_BLACK-10){
+      readLineSensors();
+      moveTo("forward");
+    }
+    motors.stop();delay(1000);
+    turnGyro(-lastYaw, "left", true);
+    motors.stop();
+    delay(5000);
+  }
+}
+
+/* FUNÇÃO PARA IDENTIFICAR O POSICIONAMENTO DO ROBÔ */
+int isInTarget(){
+  bool leftIdInTarget = false;
+  bool middleIdInTarget = false;
+  bool rightIdInTarget = false;
+  bool leftInTarget = false;
+  bool rightInTarget = false;
+
+  if(leftIdValue >= ID_BLACK-10){ leftIdInTarget = true; }
+  if(middleIdValue >= ID_BLACK-10){ middleIdInTarget = true; }
+  if(rightIdValue >= ID_BLACK-10){ rightIdInTarget = true; }
+  if(leftValue >= LEFT_WHITE){ leftInTarget = true; }
+  if(rightValue >= RIGHT_WHITE){ rightInTarget = true; }
+  
+  if(leftIdInTarget && middleIdInTarget && rightIdInTarget){ return 123; } // 3
+  else if(leftIdInTarget && middleIdInTarget){ return 12; } // 90 e
+  else if(middleIdInTarget && rightIdInTarget){ return 23; } // 90 d
+  else if(middleIdInTarget && !leftInTarget && !rightInTarget){ return 45; } // ideal
+  else if(leftInTarget || rightInTarget){ return 10; } //linha
+  else{ return 0; } //excessões
+}
+
+/* FUNÇÃO PARA IDENTIFICAR FITAS VERDES */
+int solveIntersection(){
+  if(digitalRead(LEFT_COLOR_NANO) == HIGH && digitalRead(RIGHT_COLOR_NANO) == HIGH){ return 12; }
+  else if(digitalRead(LEFT_COLOR_NANO) == HIGH){ return 1; }
+  else if(digitalRead(RIGHT_COLOR_NANO) == HIGH){ return 2; }
+  else{ return 0; }
+}
+
+void resolveIntersection(){
+  //motors.stop();
+  //delay(100);
+  switch(solveIntersection()){
+    case 1:
+      moveTo("left");
+      delay(700);
+      break;
+    case 2:
+      moveTo("right");
+      delay(700);
+      break;
+    case 12:
+      moveTo("turn180");
+      break;
+  }
+}
+
+/* FUNÇÃO PARA IDENTIFICAR REDUTORES */
+void identifyMotorStall(){
+  readGyroSensor();
+  if(yaw == lastYaw && (actualTime-motorStallLastTime) >= 5000){
+    motors.drive(55, FORWARD);
+    delay(600);
+    moveTo("backward");
+    delay(200);
+    motorStallLastTime = actualTime;
+  }else if(yaw != lastYaw){
+    Serial.println("moviemnto");
+    motorStallLastTime = actualTime;
+  }
+  lastYaw = yaw;
+}
+
+/* FUNÇÃO PARA EXECUTAR O SEGUE-LINHA */
+void lineFollower(){
+  int leftVelocity = 0, leftDirection = FORWARD;
+  int rightVelocity = 0, rightDirection = FORWARD;
+  const float Kp = 0.5;
+  
+  leftVelocity = (leftValue - LEFT_WHITE)*Kp;
+  rightVelocity = (rightValue - RIGHT_WHITE)*Kp;
+  
+  if(leftVelocity < 0){
+    leftVelocity *= -1;
+    leftDirection = BACKWARD;
+  }
+  if(rightVelocity < 0){
+    rightVelocity *= -1;
+    rightDirection = BACKWARD;
+  }
+  
+  leftVelocity = map(leftVelocity, 0, DEFAULT_SPEED, DEFAULT_SPEED, MAX_SPEED);
+  rightVelocity = map(rightVelocity, 0, DEFAULT_SPEED, DEFAULT_SPEED, MAX_SPEED);
+  
+  motors.leftDrive(rightVelocity, rightDirection);
+  motors.rightDrive(leftVelocity, leftDirection);
+}
